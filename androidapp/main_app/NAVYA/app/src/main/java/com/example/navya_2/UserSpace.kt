0e0f8f08eb8413d87cbd072b00f432487b1c8d16
YoussefMostafaMohammed <youssefmostafa453@gmail.com>
2025-07-09 00:44:46 +0300

    Make Navya as LUNCHER app

--- androidapp/main_app/NAVYA/app/src/main/java/com/example/navya_2/UserSpace.kt
+++ androidapp/main_app/NAVYA/app/src/main/java/com/example/navya_2/UserSpace.kt
@@ -1,13 +1,16 @@
 package com.example.navya_2
 
+import android.Manifest
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.UserManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresPermission
 import androidx.fragment.app.Fragment
 import java.text.SimpleDateFormat
 import java.util.*
@@ -18,9 +21,14 @@ class UserSpace : Fragment() {
     private lateinit var dateText: TextView
     private val handler = Handler(Looper.getMainLooper())
     private lateinit var updateRunnable: Runnable
+    private lateinit var usernameText: TextView
 
     companion object {
         fun newInstance() = UserSpace()
+    }
+    private fun getUsername(): String {
+        val userManager = requireContext().getSystemService(UserManager::class.java)
+        return userManager?.userName ?: "Guest"
     }
 
     override fun onCreateView(
@@ -31,11 +39,14 @@ class UserSpace : Fragment() {
 
         timeText = view.findViewById(R.id.timeText)
         dateText = view.findViewById(R.id.dateText)
+        usernameText = view.findViewById(R.id.usernameText)
+
+        usernameText.text = getUsername()
 
         startClock()
-
         return view
     }
+
 
     private fun startClock() {
         updateRunnable = object : Runnable {
